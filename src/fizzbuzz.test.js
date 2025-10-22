94a6d3e249e1b617887ddb9685581bcbfa8965e4
Carlos Prudencio <andrepruden0@gmail.com>
2025-10-21 22:48:26 -0400

    Devuelve Fizz para numero 3

---
+++ src/fizzbuzz.test.js
@@ -0,0 +1,10 @@
+describe("TestFizzBuzz", () => {
+  it("fizzbuzz deberia devolver Fizz si es el numero 3", () => {
+    expect(fizzbuzz(3)).toEqual("Fizz");
+  });
+
+});
+
+function fizzbuzz(n) {
+  return "Fizz";
+}
