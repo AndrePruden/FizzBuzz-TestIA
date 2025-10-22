9ad35415a689c17622c7caa10d1e39720f23ce47
Carlos Prudencio <andrepruden0@gmail.com>
2025-10-21 22:53:30 -0400

    Devuelve el mismo numero en caso de no ser multiplo de 3

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -3,8 +3,16 @@ describe("TestFizzBuzz", () => {
     expect(fizzbuzz(3)).toEqual("Fizz");
   });
 
+  it("fizzbuzz deberia devolver 1 si es el numero 1", () => {
+    expect(fizzbuzz(1)).toEqual("1");
+  });
+
 });
 
 function fizzbuzz(n) {
-  return "Fizz";
+    if(n%3 == 0){
+        return "Fizz";
+    }
+    return n.toString();
+  
 }
